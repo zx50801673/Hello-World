6c6ecef90ef688b00ca298fefe90267351bc525e
zx50801673 <zx50801673@163.com>
2016-08-13 23:25:42 +0800

    Revert "client1"
    This reverts commit f81a690b4400ae7778bac99074706679703f1757.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("helloWorld!");
 		System.out.println("git checkout itea-001");
-		System.out.println("test client");
 	}
 }
